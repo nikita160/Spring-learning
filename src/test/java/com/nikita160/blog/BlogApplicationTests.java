f9fc0fe9924a9cbdb684e0772fedc885b80aa8bf
Nikita <nikitax160@yandex.ru>
2022-05-17 13:07:37 +0300

    start

---
+++ src/test/java/com/nikita160/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikita160.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
